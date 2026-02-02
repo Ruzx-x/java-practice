10723f903f8574c26498b298f6b362626e4f274f
Ruzx-x <ruthxcode@gmail.com>
2026-02-02 11:45:20 +0000

    Add StringPractice class with basic string methods

---
+++ week-1/StringPractice.java
@@ -0,0 +1,7 @@
+public class StringPractice {
+  public static void main(String[] args){
+    String message = "Hello, Java!";
+    System.out.println(message.toUpperCase());
+    System.out.println("Length: " + message.length());
+  }
+}
